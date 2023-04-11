d05147b3c43f2edacffa3550ec49d71d7ce8cc21
John Mertz <git@john.me.tz>
2023-04-10 22:28:34 -0400

    More liberal usage information

--- bin/get_today_stats.pl
+++ bin/get_today_stats.pl
@@ -20,25 +20,6 @@
 #
 #
 #   This script will output the count of messages/spams/viruses for today
-#
-#   Usage:
-#           get_today_stats.pl [-aAmbsSvVcCpulL]
-#   -a: output all stats
-#   -A: output all stats in raw format
-#   -B: output all but user and domains stats in raw format
-#   -m: output number of messages processed
-#   -b: output total bytes processed
-#   -s: output number of spams detected
-#   -S: output percent of spams per messages
-#   -v: output number of viruses detected
-#   -V: output percent of viruses
-#   -c: output number of dangerous content detected
-#   -C: output percent of dangerous content
-#   -p: same as -S
-#   -u: output the number of email addresses processed by the system
-#   -l: output number of clean messages
-#   -L: output percent of clean messages
-#   -d: output number of filtered domains
 
 use v5.36;
 use strict;
@@ -57,23 +38,46 @@ my $mode_given = shift;
 my $spec = shift;
 my $mode = "B";
 
-if     (!$mode_given || $mode_given =~ /^\-a$/) { $mode = 'a';}
-elsif  ($mode_given =~ /^\-A$/) { $mode = 'A'; }
-elsif  ($mode_given =~ /^\-B$/) { $mode = 'B'; }
-elsif  ($mode_given =~ /^\-m$/) { $mode = 'm'; }
-elsif  ($mode_given =~ /^\-b$/) { $mode = 'b'; }
-elsif  ($mode_given =~ /^\-s$/) { $mode = 's'; }
-elsif  ($mode_given =~ /^\-S$/) { $mode = 'S'; }
-elsif  ($mode_given =~ /^\-v$/) { $mode = 'v'; }
-elsif  ($mode_given =~ /^\-V$/) { $mode = 'V'; }
-elsif  ($mode_given =~ /^\-c$/) { $mode = 'c'; }
-elsif  ($mode_given =~ /^\-C$/) { $mode = 'C'; }
-elsif  ($mode_given =~ /^\-p$/) { $mode = 'S'; }
-elsif  ($mode_given =~ /^\-u$/) { $mode = 'u'; }
-elsif  ($mode_given =~ /^\-l$/) { $mode = 'l'; }
-elsif  ($mode_given =~ /^\-L$/) { $mode = 'L'; }
-elsif  ($mode_given =~ /^\-d$/) { $mode = 'd'; }
-elsif  ($mode_given =~ /^[azAZ09\-\_\.\@]+$/) { $spec = $mode_given; }
+if (!$mode_given || $mode_given =~ /^\-a$/) {
+    $mode = 'a';
+} elsif ($mode_given =~ /^\-h$/) {
+    usage();
+} elsif ($mode_given =~ /^\-A$/) {
+    $mode = 'A';
+} elsif ($mode_given =~ /^\-B$/) {
+    $mode = 'B';
+} elsif ($mode_given =~ /^\-m$/) {
+    $mode = 'm';
+} elsif ($mode_given =~ /^\-b$/) {
+    $mode = 'b';
+} elsif ($mode_given =~ /^\-s$/) {
+    $mode = 's';
+} elsif ($mode_given =~ /^\-S$/) {
+    $mode = 'S';
+} elsif ($mode_given =~ /^\-v$/) {
+    $mode = 'v';
+} elsif ($mode_given =~ /^\-V$/) {
+    $mode = 'V';
+} elsif ($mode_given =~ /^\-c$/) {
+    $mode = 'c';
+} elsif ($mode_given =~ /^\-C$/) {
+    $mode = 'C';
+} elsif ($mode_given =~ /^\-p$/) {
+    $mode = 'S';
+} elsif ($mode_given =~ /^\-u$/) {
+    $mode = 'u';
+} elsif ($mode_given =~ /^\-l$/) {
+    $mode = 'l';
+} elsif ($mode_given =~ /^\-L$/) {
+    $mode = 'L';
+} elsif ($mode_given =~ /^\-d$/) {
+    $mode = 'd';
+} elsif ($mode_given =~ /^[azAZ09\-\_\.\@]+$/) {
+    $spec = $mode_given;
+} else {
+    print("Invalid selection: $mode_given\n");
+    usage();
+}
 
 my $total_bytes = 0;
 my $total_msg = 0;
@@ -92,96 +96,134 @@ my $client = StatsClient->new();
 
 my $bytes = $client->query('GET global:size');
 if ( ! defined ($bytes)  || $bytes =~ /^_/) {
-  $bytes=0;
+    $bytes=0;
 }
 $total_bytes = $bytes;
 
 my $msgs = $client->query('GET global:msg');
 if ($msgs =~ /^_/) {
-  $msgs=0;
+    $msgs=0;
 }
 $total_msg = $msgs;
 
 my $spams = $client->query('GET global:spam');
 if ($spams =~ /^_/) {
-  $spams = 0;
+    $spams = 0;
 }
 $total_spam = $spams;
 
 my $viruses = $client->query('GET global:virus');
 if ($viruses =~ /^_/) {
-  $viruses = 0;
+    $viruses = 0;
 }
 $total_virus = $viruses;
 
 my $names = $client->query('GET global:name');
 if ($names =~ /^_/) {
-  $names = 0;
+    $names = 0;
 }
 my $others = $client->query('GET global:other');
 if ($others =~ /^_/) {
-  $others = 0;
+    $others = 0;
 }
 $total_content = $names + $others;
 if ($total_msg > 0) {
-  $percentspam = int(((100/$total_msg) * $total_spam)*100)/100;
-  $percentvirus = int(((100/$total_msg) * $total_virus)*100)/100;
-  $percentcontent = int(((100/$total_msg) * $total_content)*100)/100;
+    $percentspam = int(((100/$total_msg) * $total_spam)*100)/100;
+    $percentvirus = int(((100/$total_msg) * $total_virus)*100)/100;
+    $percentcontent = int(((100/$total_msg) * $total_content)*100)/100;
 } else {
-  $percentspam = 0;
-  $percentvirus = 0;
-  $percentcontent = 0;
+    $percentspam = 0;
+    $percentvirus = 0;
+    $percentcontent = 0;
 }
 
 my $cleans = $client->query('GET global:clean');
 if ($cleans =~ /^_/) {
-  $cleans = 0;
+    $cleans = 0;
 }
 $total_clean = $cleans;
 if ($total_msg > 0) {
-  $percentclean = int(((100/$total_msg) * $total_clean)*100)/100;
+    $percentclean = int(((100/$total_msg) * $total_clean)*100)/100;
 } else {
-  $percentclean = 0;
+    $percentclean = 0;
 }
 
 my $users = $client->query('GET global:user');
 if ($users =~ /^_/) {
-  $users = 0;
+    $users = 0;
 }
 $user_count = $users;
 
 my $domains = $client->query('GET global:domain');
 if ($domains =~ /^_/) {
-  $domains = 0;
+    $domains = 0;
 }
 $domain_count = $domains;
 
 if ( ! defined ($total_bytes) ) {
-  $total_bytes = 0;
+    $total_bytes = 0;
 }
 if ($mode eq "a") {
-	print "total bytes:   $total_bytes\n";
-	print "total msgs:    $total_msg\n";
-	print "total spams:   $total_spam ($percentspam%)\n";
-	print "total viruses: $total_virus ($percentvirus%)\n";
-	print "total content: $total_content ($percentcontent%)\n";
-	print "total clean:   $total_clean ($percentclean%)\n";
+    print "total bytes:   $total_bytes\n";
+    print "total msgs:    $total_msg\n";
+    print "total spams:   $total_spam ($percentspam%)\n";
+    print "total viruses: $total_virus ($percentvirus%)\n";
+    print "total content: $total_content ($percentcontent%)\n";
+    print "total clean:   $total_clean ($percentclean%)\n";
+} elsif ($mode eq "A") {
+    print("$total_bytes|$total_msg|$total_spam|$percentspam|$total_virus|$percentvirus|$total_content|$percentcontent|$user_count|$total_clean|$percentclean|$domain_count");
+} elsif ($mode eq "B") {
+    print("$total_bytes|$total_msg|$total_spam|$percentspam|$total_virus|$percentvirus|$total_content|$percentcontent|$total_clean|$percentclean");
+} elsif ($mode eq "b") {
+    print($total_bytes);
+} elsif ($mode eq "m") {
+    print($total_msg);
+} elsif ($mode eq "s") {
+    print($total_spam);
+} elsif ($mode eq "S") {
+    print($percentspam);
+} elsif ($mode eq "v") {
+    print($total_virus);
+} elsif ($mode eq "V") {
+    print($percentvirus);
+} elsif ($mode eq "c") {
+    print($total_content);
+} elsif ($mode eq "C") {
+    print($percentcontent);
+} elsif ($mode eq "p") {
+    print($percentspam);
+} elsif ($mode eq "u") {
+    print($user_count);
+} elsif ($mode eq "l") {
+    print($total_clean);
+} elsif ($mode eq "L") {
+    print($percentclean);
+} elsif ($mode eq "d") {
+    print($domain_count);
 }
-elsif ($mode eq "A") { print("$total_bytes|$total_msg|$total_spam|$percentspam|$total_virus|$percentvirus|$total_content|$percentcontent|$user_count|$total_clean|$percentclean|$domain_count");}
-elsif ($mode eq "B") { print("$total_bytes|$total_msg|$total_spam|$percentspam|$total_virus|$percentvirus|$total_content|$percentcontent|$total_clean|$percentclean");}
-elsif ($mode eq "b") { print($total_bytes); }
-elsif ($mode eq "m") { print($total_msg); }
-elsif ($mode eq "s") { print($total_spam); }
-elsif ($mode eq "S") { print($percentspam); }
-elsif ($mode eq "v") { print($total_virus); }
-elsif ($mode eq "V") { print($percentvirus); }
-elsif ($mode eq "c") { print($total_content); }
-elsif ($mode eq "C") { print($percentcontent); }
-elsif ($mode eq "p") { print($percentspam); }
-elsif ($mode eq "u") { print($user_count); }
-elsif ($mode eq "l") { print($total_clean); }
-elsif ($mode eq "L") { print($percentclean); }
-elsif ($mode eq "d") { print($domain_count); }
 
-exit 0;
-
+sub usage
+{
+    print(
+"Usage:
+    get_today_stats.pl [-aAmbsSvVcCpulLh]
+        -a: output all stats
+        -A: output all stats in raw format
+        -B: output all but user and domains stats in raw format
+        -m: output number of messages processed
+        -b: output total bytes processed
+        -s: output number of spams detected
+        -S: output percent of spams per messages
+        -v: output number of viruses detected
+        -V: output percent of viruses
+        -c: output number of dangerous content detected
+        -C: output percent of dangerous content
+        -p: same as -S
+        -u: output the number of email addresses processed by the system
+        -l: output number of clean messages
+        -L: output percent of clean messages
+        -d: output number of filtered domains
+        -h: this menu\n"
+    );
+    exit(1);
+}
