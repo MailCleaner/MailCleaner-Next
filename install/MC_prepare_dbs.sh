a5eb53d08326e7b5d40011f6103129f7d05cb1b3
John Mertz <git@john.me.tz>
2024-03-25 13:12:06 -0600

    Check inputs and fix database creation

--- install/MC_prepare_dbs.sh
+++ install/MC_prepare_dbs.sh
@@ -2,25 +2,42 @@
 
 if [ -z "$SRCDIR" ]; then
 	SRCDIR=$(grep 'SRCDIR' /etc/mailcleaner.conf | cut -d ' ' -f3)
-	if [ "SRCDIR" = "" ]; then
+	if [[ "$SRCDIR" -eq "" ]]; then
 		SRCDIR=/usr/mailcleaner
 	fi
 fi
 if [ -z "$VARDIR" ]; then
 	VARDIR=$(grep 'VARDIR' /etc/mailcleaner.conf | cut -d ' ' -f3)
-	if [ "VARDIR" = "" ]; then
+	if [[ "$VARDIR" = "" ]]; then
 		VARDIR=/var/mailcleaner
 	fi
 fi
-if [ "$ORGANIZATION" = "" ]; then
-	ORGANIZATION=$(grep 'ORGANIZATION' /etc/mailcleaner.conf | cut -d ' ' -f3-)
+if [ -z "$MCHOSTNAME" ]; then
 	MCHOSTNAME=$(grep 'MCHOSTNAME' /etc/mailcleaner.conf | cut -d ' ' -f3)
+fi
+if [ -z "$HOSTID" ]; then
 	HOSTID=$(grep 'HOSTID' /etc/mailcleaner.conf | cut -d ' ' -f3)
+fi
+if [ -z "$CLIENTID" ]; then
 	CLIENTID=$(grep 'CLIENTID' /etc/mailcleaner.conf | cut -d ' ' -f3)
+fi
+if [ -z "$DEFAULTDOMAIN" ]; then
 	DEFAULTDOMAIN=$(grep 'DEFAULTDOMAIN' /etc/mailcleaner.conf | cut -d ' ' -f3)
+fi
+if [ -z "$CLIENTTECHMAIL" ]; then
 	CLIENTTECHMAIL=$(grep 'CLIENTTECHMAIL' /etc/mailcleaner.conf | cut -d ' ' -f3)
+fi
+if [ -z "$MYMAILCLEANERPWD" ]; then
 	MYMAILCLEANERPWD=$(grep 'MYMAILCLEANERPWD' /etc/mailcleaner.conf | cut -d ' ' -f3)
+fi
+if [ -z "$ISMASTER" ]; then
 	ISMASTER=$(grep 'ISMASTER' /etc/mailcleaner.conf | cut -d ' ' -f3)
+	if [[ "$ISMASTER" -eq "" ]]; then
+		ISMASTER=Y
+	fi
+fi
+if [ -z "$ORGANIZATION" ]; then
+	ORGANIZATION=$(grep 'ORGANIZATION' /etc/mailcleaner.conf | cut -d ' ' -f3-)
 fi
 
 cd $SRCDIR/install
@@ -58,23 +75,19 @@ if [ -z $MYMAILCLEANERPWD ]; then
 	echo ""
 fi
 
-echo "Generating slave database"
-/usr/bin/mariadb-install-db --datadir=${VARDIR}/spool/mysql_slave --defaults-file=$SRCDIR/etc/mysql/my_slave.cnf 2>&1 >/dev/null
-
-
-echo "Generating master database"
-/usr/bin/mariadb-install-db --datadir=${VARDIR}/spool/mysql_master --defaults-file=$SRCDIR/etc/mysql/my_master.cnf 2>&1 >/dev/null
-
 echo "Creating configuration files and directories"
 $SRCDIR/bin/dump_mysql_config.pl 2>&1
 
-echo "Starting MariaDB"
-systemctl start mariadb@master
-systemctl start mariadb@slave
-sleep 1
+for SOCKDIR in master slave; do
+	echo "Generating $SOCKDIR database"
+	/usr/bin/mariadb-install-db --datadir=${VARDIR}/spool/mysql_${SOCKDIR} --defaults-file=$SRCDIR/etc/mysql/my_${SOCKDIR}.cnf 2>&1 >/dev/null
 
-echo "Re-configuring databases and users"
-cat >/tmp/tmp_install.sql <<EOF
+	echo "Starting MariaDB ${SOCKDIR}"
+	systemctl start mariadb@${SOCKDIR}
+	sleep 3
+
+	echo "Re-configuring mysql_$SOCKDIR databases and users"
+	/usr/bin/mariadb -u root -S ${VARDIR}/run/mysql_${SOCKDIR}/mysqld.sock <<EOF
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYROOTPWD';
 USE mysql;
 DELETE FROM user WHERE User='';
@@ -90,46 +103,41 @@ CREATE DATABASE mc_stats;
 CREATE DATABASE dmarc_reporting;
 DELETE FROM user WHERE User='mailcleaner';
 DELETE FROM db WHERE User='mailcleaner';
+CREATE USER 'mailcleaner' IDENTIFIED BY '$MYMAILCLEANERPWD';
 GRANT ALL PRIVILEGES ON mc_config.* TO mailcleaner@"%" IDENTIFIED BY '$MYMAILCLEANERPWD' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON mc_spool.* TO mailcleaner@"%" IDENTIFIED BY '$MYMAILCLEANERPWD' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON mc_stats.* TO mailcleaner@"%" IDENTIFIED BY '$MYMAILCLEANERPWD' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON dmarc_reporting.* TO mailcleaner@"%" IDENTIFIED BY '$MYMAILCLEANERPWD' WITH GRANT OPTION;
 GRANT RELOAD, SLAVE MONITOR, REPLICATION SLAVE, REPLICATION SLAVE ADMIN, REPLICATION CLIENT ON *.* TO "mailcleaner"@"%";
-ALTER USER 'mailcleaner' IDENTIFIED BY '$MYROOTPWD';
 FLUSH PRIVILEGES;
 EOF
-
-/usr/bin/mariadb -S ${VARDIR}/run/mysql_master/mysqld.sock </tmp/tmp_install.sql 2>&1
-/usr/bin/mariadb -S ${VARDIR}/run/mysql_slave/mysqld.sock </tmp/tmp_install.sql 2>&1
-
-#rm /tmp/tmp_install.sql 2>&1
-
-for SOCKDIR in mysql_slave mysql_master; do
+	echo "Initializing $SOCKDIR tables..."
 	for file in $(find dbs -name t_cf*); do
 		echo "Loading $file in $SOCKDIR"
-		/usr/bin/mariadb -umailcleaner -p$MYMAILCLEANERPWD -S$VARDIR/run/$SOCKDIR/mysqld.sock mc_config <$file
+		/usr/bin/mariadb -umailcleaner -p${MYMAILCLEANERPWD} -S${VARDIR}/run/mysql_${SOCKDIR}/mysqld.sock mc_config <$file
 	done
 	for file in $(find dbs/spam -name t_sp*); do
 		echo "Loading $file in $SOCKDIR"
-		/usr/bin/mariadb -umailcleaner -p$MYMAILCLEANERPWD -S$VARDIR/run/$SOCKDIR/mysqld.sock mc_spool <$file
+		/usr/bin/mariadb -umailcleaner -p${MYMAILCLEANERPWD} -S${VARDIR}/run/mysql_${SOCKDIR}/mysqld.sock mc_spool <$file
 	done
 	for file in $(find dbs -name t_sp*); do
 		echo "Loading $file in $SOCKDIR"
-		/usr/bin/mariadb -umailcleaner -p$MYMAILCLEANERPWD -S$VARDIR/run/$SOCKDIR/mysqld.sock mc_spool <$file
+		/usr/bin/mariadb -umailcleaner -p${MYMAILCLEANERPWD} -S${VARDIR}/run/mysql_${SOCKDIR}/mysqld.sock mc_spool <$file
 	done
 	for file in $(find dbs -name t_st*); do
 		echo "Loading $file in $SOCKDIR"
-		/usr/bin/mariadb -umailcleaner -p$MYMAILCLEANERPWD -S$VARDIR/run/$SOCKDIR/mysqld.sock mc_stats <$file
+		/usr/bin/mariadb -umailcleaner -p${MYMAILCLEANERPWD} -S${VARDIR}/run/mysql_${SOCKDIR}/mysqld.sock mc_stats <$file
 	done
+	echo "Checking and repairing $SOCKDIR databases"
+	if [[ $SOCKDIR -eq 'master' ]]; then
+		${SRCDIR}/bin/check_db.pl -m --update 2>&1
+		${SRCDIR}/bin/check_db.pl -m --myrepair 2>&1
+		/usr/bin/mariadb -umailcleaner -p${MYMAILCLEANERPWD} -S${VARDIR}/run/mysql_master/mysqld.sock dmarc_reporting <dbs/dmarc_reporting.sql
+	else
+		${SRCDIR}/bin/check_db.pl -s --update 2>&1
+		${SRCDIR}/bin/check_db.pl -s --myrepair 2>&1
+	fi
 done
-
-echo "Checking and repairing databases"
-$SRCDIR/bin/check_db.pl -m --update 2>&1
-$SRCDIR/bin/check_db.pl -s --update 2>&1
-$SRCDIR/bin/check_db.pl -m --myrepair 2>&1
-$SRCDIR/bin/check_db.pl -s --myrepair 2>&1
-
-/usr/bin/mariadb -umailcleaner -p$MYMAILCLEANERPWD -S$VARDIR/run/mysql_master/mysqld.sock dmarc_reporting <dbs/dmarc_reporting.sql
 
 ## TO DO: check these values !! either coming from the superior installation script or from /etc/mailcleaner.conf
 
@@ -153,7 +161,6 @@ echo "INSERT INTO master (hostname, password, ssh_pub_key) VALUES ('$MASTERHOST'
 echo "INSERT INTO httpd_config (serveradmin, servername) VALUES('$CLIENTTECHMAIL', '$MCHOSTNAME');" | /usr/bin/mariadb -umailcleaner -p$MYMAILCLEANERPWD -S$VARDIR/run/mysql_master/mysqld.sock mc_config
 
 echo "Configuring replication"
-echo $MASTERPASSWD
 echo "STOP SLAVE; CHANGE MASTER TO master_host='$MASTERHOST', master_user='mailcleaner', master_password='$MASTERPASSWD'; START SLAVE;" | /usr/bin/mariadb -umailcleaner -p$MYMAILCLEANERPWD -S$VARDIR/run/mysql_slave/mysqld.sock mc_config
 
 echo "Restarting Slave DB"
