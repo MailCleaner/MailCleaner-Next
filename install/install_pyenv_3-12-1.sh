a013adc3249f3b59372281901b003517b8e8ba20
John Mertz <git@john.me.tz>
2024-08-26 17:56:03 -0600

    Remove unneeded versions and OpenSSL linking

--- install/install_pyenv_3-12-1.sh
+++ install/install_pyenv_3-12-1.sh
@@ -27,29 +27,58 @@ VARDIR="/var/mailcleaner"
 cd $VARDIR
 
 if [ -f $VARDIR/log/mailcleaner/install_pyenv.log ]; then
-	rm $VARDIR/log/mailcleaner/install_pyenv.log
+    rm $VARDIR/log/mailcleaner/install_pyenv.log
 fi
 
 FREE_SPACE=$(df -k /var/mailcleaner | tail -1 | awk '{print $4}')
 
 if [ $FREE_SPACE -lt 600000 ]; then
-	echo "[Errno 1]: Not enough disk space" >>$VARDIR/log/mailcleaner/install_pyenv.log
-	exit
+    echo "[Errno 1]: Not enough disk space" >>$VARDIR/log/mailcleaner/install_pyenv.log
+    exit
 fi
 
-git clone https://github.com/pyenv/pyenv.git .pyenv
+if [ -d .pyenv ]; then
+    echo "$VARDIR/.pyenv already exists"
+    if [ -d $VARDIR/.pyenv/.git ]; then
+        echo "Pulling latest from Git..."
+        cd $VARDIR/.pyenv
+        git pull
+        cd ..
+    else
+        echo "$VARDIR/.pyenv is not a git repository. Removing and reinstalling..."
+        rm -rf $VARDIR/.pyenv
+        git clone https://github.com/pyenv/pyenv.git .pyenv
+    fi
+else
+    git clone https://github.com/pyenv/pyenv.git .pyenv
+fi
+
 export PYENV_ROOT="$VARDIR/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init --path)"
-LD_LIBRARY_PATH="${HOME}/lib/openssl/lib" LDFLAGS="-L${HOME}/lib/openssl/lib -Wl,-rpath,${HOME}/lib/openssl/lib" CFLAGS="-I$HOME/lib/openssl/include" SSH="$HOME/lib/openssl" pyenv install 3.12.1 -s
-pyenv local 3.12.1
+pyenv install 3.12.1 -s
+for i in $(pyenv versions | sed 's/* //' | awk '{ print $1 }'); do
+    case "$i" in
+        'system')
+            ;;
+        '3.12.1')
+            ;;
+        *)
+            echo "Removing unneeded version $i"
+            pyenv uninstall -f $i
+            ;;
+    esac
+done
+pyenv global 3.12.1
+
+pip install --upgrade pip
 
 pip install mailcleaner-library --trusted-host repository.mailcleaner.net --index https://repository.mailcleaner.net/python/ --extra-index https://pypi.org/simple/
 
 IMPORT_MC_LIB=$(python -c "import mailcleaner")
 if [ $? -eq 1 ]; then
-	echo "[Errno 4]: Can't import mailcleaner" >>$VARDIR/log/mailcleaner/install_pyenv.log
-	exit
+    echo "[Errno 4]: Can't import mailcleaner" >>$VARDIR/log/mailcleaner/install_pyenv.log
+    exit
 fi
 
 echo "[Errno 0]: Everything went fine..." >>$VARDIR/log/mailcleaner/install_pyenv.log
