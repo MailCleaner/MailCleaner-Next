b519e294831ab075c835c439a5adbc70cd1ad42c
John Mertz <git@john.me.tz>
2024-08-26 20:15:09 -0600

    Fix equivalency symbol and .bashrc integration

--- install/install.sh
+++ install/install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 if [ -z "$LOGFILE" ]; then
-	LOGFILE=/tmp/mailcleaner.log
+    LOGFILE=/tmp/mailcleaner.log
 fi
 
 # Create config file if it does not exist
 if [ -z "$CONFFILE" ]; then
-	CONFFILE=/etc/mailcleaner.conf
+    CONFFILE=/etc/mailcleaner.conf
 fi
 
 if [ ! -f $CONFFILE ]; then
-	MCHOSTNAME=$(hostname)
-	cat >$CONFFILE <<EOF
+    MCHOSTNAME=$(hostname)
+    cat >$CONFFILE <<EOF
 SRCDIR = /usr/mailcleaner
 VARDIR = /var/mailcleaner
 MCHOSTNAME = $MCHOSTNAME
@@ -27,118 +27,125 @@ fi
 
 # Setup missing vars
 if [ -z $SRCDIR ]; then
-	SRCDIR=$(grep 'SRCDIR' /etc/mailcleaner.conf | cut -d ' ' -f3)
-	if [[ "$SRCDIR" -eq "" ]]; then
-		SRCDIR="/usr/mailcleaner"
-		echo "SRCDIR = $SRCDIR" >>$CONFFILE
-	fi
+    SRCDIR=$(grep 'SRCDIR' /etc/mailcleaner.conf | cut -d ' ' -f3)
+    if [[ "$SRCDIR" == "" ]]; then
+        SRCDIR="/usr/mailcleaner"
+        echo "SRCDIR = $SRCDIR" >>$CONFFILE
+    fi
 fi
 export SRCDIR
 if [ -z $VARDIR ]; then
-	VARDIR=$(grep 'VARDIR' /etc/mailcleaner.conf | cut -d ' ' -f3)
-	if [[ "$VARDIR" -eq "" ]]; then
-		VARDIR="/var/mailcleaner"
-		echo "VARDIR = $VARDIR" >>$CONFFILE
-	fi
+    VARDIR=$(grep 'VARDIR' /etc/mailcleaner.conf | cut -d ' ' -f3)
+    if [[ "$VARDIR" == "" ]]; then
+        VARDIR="/var/mailcleaner"
+        echo "VARDIR = $VARDIR" >>$CONFFILE
+    fi
 fi
 export VARDIR
 
 if [ -z "$MYMAILCLEANERPWD" ]; then
-	MYMAILCLEANERPWD="$(grep MYMAILCLEANERPWD /etc/mailcleaner.conf | cut -d' ' -f3-)"
-	if [[ "$MYMAILCLEANERPWD" -eq "" ]]; then
-		MYMAILCLEANERPWD=$(pwgen -1)
-		echo "MYMAILCLEANERPWD = $MYMAILCLEANERPWD" >>$CONFFILE
-	fi
+    MYMAILCLEANERPWD="$(grep MYMAILCLEANERPWD /etc/mailcleaner.conf | cut -d' ' -f3-)"
+    if [[ "$MYMAILCLEANERPWD" == "" ]]; then
+        MYMAILCLEANERPWD=$(pwgen -1)
+        echo "MYMAILCLEANERPWD = $MYMAILCLEANERPWD" >>$CONFFILE
+    fi
 fi
 export MYMAILCLEANERPWD
 export MYROOTPWD=$MYMAILCLEANERPWD
 if [ -z "$WEBADMINPWD" ]; then
-	WEBADMINPWD=$MYMAILCLEANERWPD
+    WEBADMINPWD=$MYMAILCLEANERWPD
 fi
 export WEBADMINPWD
 if [ -z "$HOSTID" ]; then
-	HOSTID=1
+    HOSTID=1
 fi
 export HOSTID
 if [ -z "$CLIENTID" ]; then
-	CLIENTID=0
+    CLIENTID=0
 fi
 export CLIENTID
 if [ -z "$ORGANIZATION" ]; then
-	ORGANIZATION="Anonymous"
+    ORGANIZATION="Anonymous"
 fi
 export ORGANIZATION
 if [ -z "$MCHOSTNAME" ]; then
-	MCHOSTNAME=$(hostname)
+    MCHOSTNAME=$(hostname)
 fi
 export MCHOSTNAME
 if [ -z "$DEFAULTDOMAIN" ]; then
-	DEFAULTDOMAIN=''
+    DEFAULTDOMAIN=''
 fi
 export DEFAULTDOMAIN
 if [ -z "$CLIENTTECHMAIL" ]; then
-	if [[ "$DEFAULTDOMAIN" -ne "" ]]; then
-		CLIENTTECHMAIL="postmaster@$DEFAULTDOMAIN"
-	else
-		CLIENTTECHMAIL='root@localhost'
-	fi
+    if [[ "$DEFAULTDOMAIN" -ne "" ]]; then
+        CLIENTTECHMAIL="postmaster@$DEFAULTDOMAIN"
+    else
+        CLIENTTECHMAIL='root@localhost'
+    fi
 fi
 export CLIENTTECHMAIL
 
 # Configure .bashrc
 if [[ -e /root/.bashrc ]]; then
-	if grep -Pq "source \${SRCDIR}/.bashrc" /root/.bashrc; then
-		echo "Updating /root/.bashrc"
-		mv /root/.bashrc /root/.bashrc_preexisting
-	fi
+    if ! grep -q 'source ${SRCDIR}/.bashrc' <<< $(cat /root/.bashrc); then
+        echo "Updating /root/.bashrc"
+        mv /root/.bashrc /root/.bashrc_preexisting
+    fi
 fi
 if [[ ! -e /root/.bashrc ]]; then
-	cat >/root/.bashrc <<EOF
-SRCDIR=\$(grep 'SRCDIR' /etc/mailcleaner.conf | cut -d ' ' -f3)
-if [[ -z \$SRCDIR ]]; then
-	SRCDIR="/usr/mailcleaner"
-fi
-export \$SRCDIR
-
+    cat >/root/.bashrc <<EOF
 # If you had a .bashrc file before installing mailcleaner, it is now sourced here
 if [ -e /root/.bashrc_preexisting ]; then
-	source /root/.bashrc_preexisting
+    source /root/.bashrc_preexisting
 fi
+
+SRCDIR=\$(grep 'SRCDIR' /etc/mailcleaner.conf | cut -d ' ' -f3)
+if [[ -z \$SRCDIR ]]; then
+    SRCDIR="/usr/mailcleaner"
+fi
+export \$SRCDIR
 
 # Import environment from MailCleaner
 source \${SRCDIR}/.bashrc
 EOF
+
+    cat >/root/.profile <<EOF
+# Import environment from MailCleaner
+source ${SRCDIR}/.bashrc
+EOF
+
 fi
 
 # Install `docker`
 if [[ "$(which docker)" == "" ]]; then
-	if [[ "$(find /etc/apt/sources.list.d -name docker*)" == "" ]] || [[ "$(grep -R download.docker.com /etc/apt/sources.list.d)" == "" ]]; then
-		echo "Adding Docker repository..."
-		curl -fsSL https://download.docker.com/linux/debian/gpg >/etc/apt/trusted.gpg.d/docker.asc
-		ARCH=$(uname -r | sed "s/^.*\-\([^\-]*\)/\1/")
-		cat >/etc/apt/sources.list.d/docker.list <<EOF
+    if [[ "$(find /etc/apt/sources.list.d -name docker*)" == "" ]] || [[ "$(grep -R download.docker.com /etc/apt/sources.list.d)" == "" ]]; then
+        echo "Adding Docker repository..."
+        curl -fsSL https://download.docker.com/linux/debian/gpg >/etc/apt/trusted.gpg.d/docker.asc
+        ARCH=$(uname -r | sed "s/^.*\-\([^\-]*\)/\1/")
+        cat >/etc/apt/sources.list.d/docker.list <<EOF
     deb [arch=$ARCH] https://download.docker.com/linux/debian bookworm stable
 EOF
-	fi
-	echo "Installing Docker..."
-	apt-get update 2>&1 >/dev/null
-	apt-get --assume-yes install docker-ce docker-ce-rootless-extras
+    fi
+    echo "Installing Docker..."
+    apt-get update 2>&1 >/dev/null
+    apt-get --assume-yes install docker-ce docker-ce-rootless-extras
 fi
+exit
 
 if [ ! -e /opt/MailScanner ]; then
-	echo "Installing MailScanner..."
-	$SRCDIR/install/mailscanner/install.sh -y
+    echo "Installing MailScanner..."
+    $SRCDIR/install/mailscanner/install.sh -y
 fi
 
 ###############################################
 ### creating users: mailcleaner, mailscanner (all others provided by packages)
 if [ "$(grep 'mailcleaner' /etc/passwd)" = "" ]; then
-	groupadd mailcleaner 2>&1 >>$LOGFILE
-	useradd -d $VARDIR -s /bin/bash -c "MailCleaner User" -g mailcleaner mailcleaner 2>&1 >>$LOGFILE
+    groupadd mailcleaner 2>&1 >>$LOGFILE
+    useradd -d $VARDIR -s /bin/bash -c "MailCleaner User" -g mailcleaner mailcleaner 2>&1 >>$LOGFILE
 fi
 if [ "$(grep 'mailscanner' /etc/passwd)" = "" ]; then
-	groupadd mailscanner 2>&1 >>$LOGFILE
-	useradd -d $VARDIR -s /bin/bash -c "MailScanner" -g mailscanner mailscanner 2>&1 >>$LOGFILE
+    groupadd mailscanner 2>&1 >>$LOGFILE
+    useradd -d $VARDIR -s /bin/bash -c "MailScanner" -g mailscanner mailscanner 2>&1 >>$LOGFILE
 fi
 
 ###############################################
@@ -155,36 +162,36 @@ $SRCDIR/install/MC_create_vars.sh 2>&1 >>$LOGFILE
 ###############################################
 ## generate ssh keys
 if [ ! -d $VARDIR/.ssh ]; then
-	mkdir $VARDIR/.ssh
+    mkdir $VARDIR/.ssh
 fi
 
 if [ ! -e $VARDIR/.ssh/id_ed25519 ]; then
-	ssh-keygen -q -t ed25519 -f $VARDIR/.ssh/id_ed25519 -N ""
-	chown -R mailcleaner:mailcleaner $VARDIR/.ssh
+    ssh-keygen -q -t ed25519 -f $VARDIR/.ssh/id_ed25519 -N ""
+    chown -R mailcleaner:mailcleaner $VARDIR/.ssh
 fi
 export HOSTKEY=$(cat $VARDIR/.ssh/id_ed25519.pub)
 
 if [ -z $ISMASTER ]; then
-	ISMASTER="Y"
+    ISMASTER="Y"
 fi
 export ISMASTER
 if [[ "$ISMASTER" -eq "Y" ]]; then
-	MASTERHOST=127.0.0.1
-	MASTERKEY=$(cat $VARDIR/.ssh/id_ed25519.pub)
-	MASTERPASSWD=$MYMAILCLEANERPWD
+    MASTERHOST=127.0.0.1
+    MASTERKEY=$(cat $VARDIR/.ssh/id_ed25519.pub)
+    MASTERPASSWD=$MYMAILCLEANERPWD
 else
-	if [ -z $MASTERHOST ]; then
-		echo "Missing MASTERHOST for slave node"
-		exit
-	fi
-	if [ -z $MASTERKEY ]; then
-		echo "Missing MASTERKEY for slave node"
-		exit
-	fi
-	if [ -z $MASTERPASSWD ]; then
-		echo "Missing MASTERPASSWD for slave node"
-		exit
-	fi
+    if [ -z $MASTERHOST ]; then
+        echo "Missing MASTERHOST for slave node"
+        exit
+    fi
+    if [ -z $MASTERKEY ]; then
+        echo "Missing MASTERKEY for slave node"
+        exit
+    fi
+    if [ -z $MASTERPASSWD ]; then
+        echo "Missing MASTERPASSWD for slave node"
+        exit
+    fi
 fi
 export MASTERHOST
 export MASTERKEY
@@ -197,13 +204,13 @@ systemctl start mariadb@master
 sleep 1
 DBEXISTS=$(echo 'SELECT * FROM system_conf;' | mc_mysql -m mc_config 2>/dev/null)
 if [ -n $FORCEDBREINSTALL ]; then
-	echo "Forcing reinstallation of databases..."
-	unset DBEXISTS
+    echo "Forcing reinstallation of databases..."
+    unset DBEXISTS
 fi
 if [ -z "$DBEXISTS" ]; then
-	echo "Creating databases..."
-	$SRCDIR/install/MC_prepare_dbs.sh 2>&1 >>$LOGFILE
-	systemctl restart mariadb@slave 2>&1 >>$LOGFILE
+    echo "Creating databases..."
+    $SRCDIR/install/MC_prepare_dbs.sh 2>&1 >>$LOGFILE
+    systemctl restart mariadb@slave 2>&1 >>$LOGFILE
 fi
 
 ###############################################
@@ -233,18 +240,18 @@ fi
 ## import default certificate
 KEYFILE=$SRCDIR/etc/apache/certs/default.key
 if [ ! -e $KEYFILE ]; then
-	echo Generating self-signing key
-	openssl genpkey -algorithm ED25519 -out $KEYFILE 2>&1 >$LOGFILE
+    echo Generating self-signing key
+    openssl genpkey -algorithm ED25519 -out $KEYFILE 2>&1 >$LOGFILE
 fi
 CERTFILE=$SRCDIR/etc/apache/certs/default.crt
 if [ ! -e $CERTFILE ]; then
-	echo Generating self-signed certificate for HTTPS/STARTTLS
-	cp $SRCDIR/etc/apache/default.conf_template /tmp/default.conf
-	sed -i "s/__HOSTNAME__/$MCHOSTNAME/" /tmp/default.conf
-	sed -i "s/__CLIENTTECHMAIL__/$CLIENTTECHMAIL/" /tmp/default.conf
-	sed -i "s/__ORGANIZATION__/$ORGANIZATION/" /tmp/default.conf
-	openssl req -new -out /tmp/default.csr -key $KEYFILE -config /tmp/default.conf 2>&1 >$LOGFILE
-	openssl x509 -req -days 3650 -in /tmp/default.csr -signkey $KEYFILE -out $CERTFILE 2>&1 >$LOGFILE
+    echo Generating self-signed certificate for HTTPS/STARTTLS
+    cp $SRCDIR/etc/apache/default.conf_template /tmp/default.conf
+    sed -i "s/__HOSTNAME__/$MCHOSTNAME/" /tmp/default.conf
+    sed -i "s/__CLIENTTECHMAIL__/$CLIENTTECHMAIL/" /tmp/default.conf
+    sed -i "s/__ORGANIZATION__/$ORGANIZATION/" /tmp/default.conf
+    openssl req -new -out /tmp/default.csr -key $KEYFILE -config /tmp/default.conf 2>&1 >$LOGFILE
+    openssl x509 -req -days 3650 -in /tmp/default.csr -signkey $KEYFILE -out $CERTFILE 2>&1 >$LOGFILE
 fi
 KEY=$(cat $KEYFILE)
 CERT=$(cat $CERTFILE)
@@ -259,35 +266,35 @@ echo "$QUERY" | $SRCDIR/bin/mc_mysql -m 2>&1 >>$LOGFILE
 ### installing mailcleaner cron job
 echo "Installing scheduled jobs..."
 if [[ ! -d /var/spool/cron/crontabs ]]; then
-	mkdir -p /var/spool/cron/crontabs
+    mkdir -p /var/spool/cron/crontabs
 fi
 if [[ ! -e /var/spool/cron/crontabs/root ]]; then
-	ln -s $SRCDIR/scripts/cron/crontab/root /var/spool/cron/crontabs/root
-	crontab /var/spool/cron/crontabs/root 2>&1 >>$LOGFILE
+    ln -s $SRCDIR/scripts/cron/crontab/root /var/spool/cron/crontabs/root
+    crontab /var/spool/cron/crontabs/root 2>&1 >>$LOGFILE
 fi
 if [[ ! -e /var/spool/cron/crontabs/mailcleaner ]]; then
-	ln -s $SRCDIR/scripts/cron/crontab/mailcleaner /var/spool/cron/crontabs/mailcleaner
+    ln -s $SRCDIR/scripts/cron/crontab/mailcleaner /var/spool/cron/crontabs/mailcleaner
 fi
 /etc/init.d/cron restart 2>&1 >>$LOGFILE
 
 ###############################################
 ### installing `pyenv`
 if [[ ! -d $VARDIR ]]; then
-	mkdir $VARDIR
+    mkdir $VARDIR
 fi
 cd $VARDIR
 if [[ ! -d .pyenv ]]; then
-	echo "Installing Pyenv..."
-	git clone --depth=1 https://github.com/pyenv/pyenv.git .pyenv 2>&1 >$LOGFILE
-	cd .pyenv
+    echo "Installing Pyenv..."
+    git clone --depth=1 https://github.com/pyenv/pyenv.git .pyenv 2>&1 >$LOGFILE
+    cd .pyenv
 else
-	cd .pyenv
-	git pull --rebase origin master 2>&1 >$LOGFILE
+    cd .pyenv
+    git pull --rebase origin master 2>&1 >$LOGFILE
 fi
 export PYENV_ROOT="$VARDIR/.pyenv"
 if ! grep -q $PYENV_ROOT <<<$(echo $PATH); then
-	echo "Adding $PYENV_ROOT to $PATH, but this should be included in $SRCDIR/.bashrc."
-	export PATH="$PYENV_ROOT/bin:$PATH"
+    echo "Adding $PYENV_ROOT to $PATH, but this should be included in $SRCDIR/.bashrc."
+    export PATH="$PYENV_ROOT/bin:$PATH"
 fi
 eval "$(pyenv init --path)"
 pyenv install 3.11.2 -s 2>&1 >$LOGFILE
@@ -298,8 +305,8 @@ pip install mailcleaner-library --trusted-host repository.mailcleaner.net --inde
 
 IMPORT_MC_LIB=$(python -c "import mailcleaner")
 if [ $? -eq 1 ]; then
-	echo "Failed to install MailCleaner Library. Not imported."
-	#exit
+    echo "Failed to install MailCleaner Library. Not imported."
+    #exit
 fi
 
 ###############################################
@@ -308,8 +315,8 @@ rm /etc/systemd/system/default.target
 rm /lib/systemd/system/default.target
 ln -s /usr/mailcleaner/scripts/systemd/mailcleaner.target /lib/systemd/system/default.target
 if [ $! ]; then
-	echo -e "\b\b\b x "
-	ERRORS="${ERRORS}
+    echo -e "\b\b\b x "
+    ERRORS="${ERRORS}
 x Failed to symlink mailcleaner.target to /lib/systemd/system/default.target"
 fi
 
@@ -317,11 +324,11 @@ echo "Starting..."
 systemctl start mailcleaner.target
 
 for i in $($SRCDIR/bin/get_status.pl -s | tr '|' ' '); do
-	if [ $i ]; then
-		echo "Not all services started successfully. Please review $LOGFILE and run installation again when problems are resolved."
-		$SRCDIR/bin/get_status.pl -s -v
-		exit 255
-	fi
+    if [ $i ]; then
+        echo "Not all services started successfully. Please review $LOGFILE and run installation again when problems are resolved."
+        $SRCDIR/bin/get_status.pl -s -v
+        exit 255
+    fi
 done
 touch $VARDIR/run/first-time-configuration
 echo "Success!"
